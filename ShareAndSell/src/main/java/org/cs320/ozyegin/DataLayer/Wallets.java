d2c4c4aaf903ce8071dcc7e1044d09b396021a22
utku.ayten <103459721+utkuayten@users.noreply.github.com>
2023-12-14 16:56:55 +0300

    create other databases

--- ShareAndSell/src/main/java/org/cs320/ozyegin/DataLayer/Wallets.java
+++ ShareAndSell/src/main/java/org/cs320/ozyegin/DataLayer/Wallets.java
@@ -1,4 +1,21 @@
 package org.cs320.ozyegin.DataLayer;
 
-public class Wallets {
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Wallets extends DbIntegration{
+    public Wallets() throws ClassNotFoundException, SQLException {
+        super("Wallets");
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS Wallets (" +
+                                "    id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "    owner VARCHAR[64]," +
+                                "    balance VARCHAR[64]" +
+                                ");");
+        this.connection.close();
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Wallets dbWallets = new Wallets();
+    }
 }

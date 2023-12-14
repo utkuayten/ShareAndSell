3ca184489e463597766007cc761520e94c3e983d
utku.ayten <103459721+utkuayten@users.noreply.github.com>
2023-12-14 16:56:55 +0300

    create other databases

--- ShareAndSell/src/main/java/org/cs320/ozyegin/DataLayer/DbIntegration.java
+++ ShareAndSell/src/main/java/org/cs320/ozyegin/DataLayer/DbIntegration.java
@@ -2,9 +2,7 @@ package org.cs320.ozyegin.DataLayer;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbIntegration
 {

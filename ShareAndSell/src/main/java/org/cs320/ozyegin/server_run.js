08525573c4dd998795769ac031b1f618665d8b26
utku.ayten <103459721+utkuayten@users.noreply.github.com>
2023-12-20 14:04:07 +0300

    initial commit

--- ShareAndSell/src/main/java/org/cs320/ozyegin/server_run.js
+++ ShareAndSell/src/main/java/org/cs320/ozyegin/server_run.js
@@ -10,26 +10,9 @@ app.use(express.static(absolutePathToAssets));
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/UserInterfaceLayer/LoginManager/Pages/loginPage.html');
 });
-/*
-app.post('/login', (req, res) => {
-    const {username, password} = req.body; // Accessing form data sent via POST
 
-    // Check credentials, perform authentication, etc.
-    // Usually involves database interaction and authentication logic
-    // For example, validating the user's credentials against a database
-
-    // Respond based on authentication status
-    if (validCredentials(username, password)) {
-        // Successful login
-        res.send('Login successful');
-    } else {
-        // Invalid credentials
-        res.status(401).send('Invalid username or password');
-    }
-});
-*/
 // Start the server
-const PORT = 1234;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

7349f94f1433be1c1c664fb801b4c77c46098e4b
basar100basar <basaryumak56@gmail.com>
2024-01-01 23:59:24 +0300

    view details attribute inside marketplace

--- ShareAndSell/src/main/java/org/cs320/ozyegin/controller/SearchAndFilterController.java
+++ ShareAndSell/src/main/java/org/cs320/ozyegin/controller/SearchAndFilterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,7 +17,7 @@ import java.security.Principal;
 import java.util.List;
 
 @Controller
-@RequestMapping("/user/marketplace")
+//@RequestMapping("/user/marketplace")
 public class SearchAndFilterController {
 
     @Autowired
@@ -25,7 +26,7 @@ public class SearchAndFilterController {
     @Autowired
     private AdvertService advertService;
 
-    @GetMapping
+    @GetMapping("/user/marketplace")
     public String showMarketplace(
             @RequestParam(name = "query", required = false) String query,
             @RequestParam(name = "hideMine", defaultValue = "false") boolean hideMine,
@@ -55,4 +56,28 @@ public class SearchAndFilterController {
 
         return "marketplace";
     }
+
+
+
+    @GetMapping("/user/details/{id}")
+    public String showProductDetails(@PathVariable Long id, Model model, Principal principal) {
+        // Log the received ID
+        System.out.println("Received ID: " + id);
+
+        User user = userRepository.findByEmail(principal.getName());
+        model.addAttribute("user", user);
+        Advertisement advertisement = advertService.findAdvertByID(id);
+
+        System.out.println("Retrieved Advertisement: " + advertisement.toString());
+
+        model.addAttribute("advertisement", advertisement);
+
+        return "productDetails";
+    }
+
+
+
+
+
+
 }
